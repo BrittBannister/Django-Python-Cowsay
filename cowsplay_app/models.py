28348ff04e3bd3a7196100cd94e9af5929031b60
Britt <brittbannister1127@icloud.com>
2021-02-11 11:41:00 -0500

    created cow model and migrated to db.

--- cowsplay_app/models.py
+++ cowsplay_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Cow_Model(models.Model):
+    text = models.TextField(max_length=50)
+
+    def __str__(self):
+        return self.text
